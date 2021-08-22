a5bc7d247b929621245a30fbf396a6d4bd7d009f
psato <forhighlow@gmail.com>
2021-08-22 11:35:07 +0900

    add twitter timeline

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use cursive::Cursive;
-use cursive::theme::Theme;
-use cursive::theme::Color;
 use crate::view::GreenCodeView;
+use cursive::theme::Color;
+use cursive::theme::Theme;
+use cursive::Cursive;
+use egg_mode::direct::Timeline;
 
+mod twitter;
 mod view;
 
-
 fn main() {
-    let mut siv: Cursive = Cursive::default();
+    twitter();
     let size = siv.screen_size();
     let mut theme = Theme::default();
-    theme.palette.set_color("view", Color::TerminalDefault );
+    theme.palette.set_color("view", Color::TerminalDefault);
     siv.set_theme(theme);
 
     siv.add_fullscreen_layer(GreenCodeView::new(1, size));
---
+++ src/twitter.rs
@@ -0,0 +1,47 @@
+use dotenv::dotenv;
+use egg_mode;
+use std::env;
+use std::{thread, time};
+
+pub struct Timeline {
+    pub count: i32,
+    pub max_id: Option<u64>,
+    pub min_id: Option<u64>,
+    pub context: Option<String>,
+}
+
+#[tokio::main]
+async fn twitter() {
+    dotenv().ok();
+
+    let c_key = env::var("CONSUMER_KEY").expect("Please set consumer-key in .env");
+    let c_secret = env::var("CONSUMER_SECRET").expect("Please set consumer-secret in .env");
+    let a_key = env::var("ACCESS_KEY").expect("Please set access-key in .env");
+    let a_secret = env::var("ACCESS_SECRET").expect("Please set access-secret in .env");
+    let consumer_token = egg_mode::KeyPair::new(c_key, c_secret);
+    let access_token = egg_mode::KeyPair::new(a_key, a_secret);
+    let token = egg_mode::Token::Access {
+        consumer: consumer_token,
+        access: access_token,
+    };
+
+    let user = egg_mode::auth::verify_tokens(&token).await.unwrap();
+    println!("User @{}'s timeline", user.screen_name);
+    println!("**************************************************************");
+
+    // get timeline
+    let timeline = egg_mode::tweet::home_timeline(&token).with_page_size(10);
+
+    let (_timeline, feed) = timeline.older(None).await.unwrap();
+
+    /*
+    print timeline
+    */
+    for tweet in &*feed {
+        println!(
+            "<@{}> {}",
+            tweet.user.as_ref().unwrap().screen_name,
+            tweet.text
+        );
+    }
+}
